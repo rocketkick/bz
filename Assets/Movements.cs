58277359434e08f07c3668006408606d50328d09
rocketkick <grad@nate.com>
2014-10-15 17:12:55 +0900

    bz

---
+++ Assets/Movements.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movements : MonoBehaviour {
+
+	public int _speed;
+	public Vector3 _targetPosition;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/csMyBezier.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csMyBezier : MonoBehaviour {
+
+	public Bezier myBezier;
+	private float t = 0f;
+	
+	void Start()
+	{
+		//myBezier = new Bezier( new Vector3( 0f, 0f, 0f ), Random.insideUnitSphere * 2f, Random.insideUnitSphere * 2f, new Vector3( -10f, 0f, -10f ) );
+		myBezier = new Bezier( new Vector3( 0f, 0f, 0f ), new Vector3(-10f,0f,0f), new Vector3(0f,0f,0f), new Vector3( -10f, 0f, -10f ) );
+	}
+	void Update()
+	{
+		Vector3 vec = myBezier.GetPointAtTime( t );
+		transform.position = vec;
+
+		t += 0.01f;
+		if( t > 1f )
+			t = 0f;
+	}
+}
